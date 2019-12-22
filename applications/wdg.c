953d9c70f33f510fd4c18c8afea5cacb167379d5
immm <1445953226@qq.com>
2019-12-22 13:55:36 +0800

    [perfect]xwdg

--- applications/wdg.c
+++ applications/wdg.c
@@ -9,15 +9,14 @@
 #include <ulog.h>
 
 void xwdg_thread_entry(void *parameter) {
-  rt_err_t _rt = RT_EOK;~
-  rt_device_t _d = RT_NULL;
+  rt_err_t _rt = RT_EOK;
+  ~rt_device_t _d = RT_NULL;
 
   _d = rt_device_find(WDT_DEVICE_NAME);
   RT_ASSERT(RT_NULL != _d);
   _rt = rt_device_init(_d);
   RT_ASSERT(RT_EOK != _rt);
 
-  LOG_I("xwdg enabled");
   while (1) {
     _rt = rt_device_control(_d, RT_DEVICE_CTRL_WDT_KEEPALIVE, NULL);
     if (RT_EOK != _rt) {
@@ -28,8 +27,9 @@ void xwdg_thread_entry(void *parameter) {
 }
 
 void wdg_thread_init(void) {
-  rt_thread_t _tid = RT_NULL;
+  LOG_I("xwdg enabled");
 
+  rt_thread_t _tid = RT_NULL;
   _tid = rt_thread_create("app_wdg", xwdg_thread_entry, RT_NULL, 256,
                           RT_THREAD_PRIORITY_MAX - 1, 20);
   RT_ASSERT(_tid != RT_NULL);
