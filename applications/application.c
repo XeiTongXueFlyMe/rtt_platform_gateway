e63d07df4de71ca9f0fbb2b8ef663329d3f7ec0c
xieming <xie.m@elitesemicon.com>
2019-12-31 09:59:48 +0800

    [fix]修复单元测试下ulog不能打印问题

--- applications/application.c
+++ applications/application.c
@@ -9,13 +9,15 @@
 #endif
 
 int rt_application_init() {
-
 #ifdef RT_USING_FINSH
   finsh_system_init();
 #endif
 
 #ifndef UNIT_TEST
   rt_components_init();
+#else
+  extern int ulog_console_backend_init(void);
+  ulog_console_backend_init();
 #endif /*UNIT_TEST*/
 
   return 0;
