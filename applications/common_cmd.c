701023a7f4c8311118c4d69294fc3c213278affd
xieming <xie.m@elitesemicon.com>
2020-01-03 08:46:01 +0800

    [add]cmd clean

--- applications/common_cmd.c
+++ applications/common_cmd.c
@@ -3,6 +3,8 @@
 #include "rtdevice.h"
 #include "rtthread.h"
 
+#if defined(RT_USING_CONSOLE)
+
 int reboot(void) {
   __set_FAULTMASK(1);
   NVIC_SystemReset();
@@ -11,3 +13,13 @@ int reboot(void) {
 }
 
 MSH_CMD_EXPORT(reboot, reset sys);
+
+
+int clean(void){
+	rt_kprintf("%s","\033[1H\033[2J");
+	return 0;
+}	
+
+MSH_CMD_EXPORT(clean, Clear Screen CLS);
+
+#endif
